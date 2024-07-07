13e5a7c7dcb81049c62c6de3fc530e641656933b
Krishna0302 <133387126+Krishna0302@users.noreply.github.com>
2024-07-07 19:15:20 +0530

    Codemirror

--- realtime_editor/src/pages/EditorPage.js
+++ realtime_editor/src/pages/EditorPage.js
@@ -1,8 +1,49 @@
-import React from "react";
+import React, {useState} from "react";
+import Client from "../components/Client";
+import Editor from "../components/Editor";
 
 const EditorPage = () => {
+
+    const [clients, setClirnts] = useState([
+        {socketId : 1, username : "krishna"},
+        {socketId : 2, username : "John"},
+        {socketId : 3, username : "vishna"},
+        {socketId : 4, username : "Rohaan"} ]);
+
     return (
-        <h1> editor page</h1>
+        <div className="mainWrap">
+            <div className="aside">
+                <div className="asideInner">
+                    <div className="logo">
+                        <img
+                            className="logoImage"
+                            src="/c (2) (1).png"
+                            alt="logo"
+                        />
+                    </div>
+
+                    <h3>Connected</h3>
+
+                    <div className="clientsList">
+                        {clients.map((client) => (
+                            <Client
+                                key={client.socketId}
+                                username={client.username}
+                            />
+                        ))}
+                    </div>
+
+                </div>
+
+                <button className="btn copyBtn">Copy ROOM ID</button>
+                <button className="btn leaveBtn">Leave</button>
+
+            </div>
+
+            <div className="editorWrap">
+            <Editor/>
+            </div>
+        </div>
     )
 }
 
